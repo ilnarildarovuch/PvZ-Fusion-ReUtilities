a6f2bd9b51564637997a3e5da5089d894ef32ecb
Nero Dynamo <dynamixus0@outlook.com>
2024-11-26 13:38:59 +0800

    New addons added.

---
+++ Utilities/Core.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+using Il2Cpp;
+
+[assembly: MelonInfo(typeof(Utilities.Core), "Utilities Addon", "1.0.0", "dynaslash", null)]
+[assembly: MelonGame("LanPiaoPiao", "PlantsVsZombiesRH")]
+
+namespace Utilities
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
