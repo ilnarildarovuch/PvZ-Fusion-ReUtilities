5787c166f66d86ccc9734e116d15e7580bad34d6
Nero Dynamo <dynamixus0@outlook.com>
2024-11-27 18:29:24 +0800

    Seed Rain test update.

---
+++ Seed Rain Overhaul/CreatePlant_Patch.cs
@@ -0,0 +1,29 @@
+﻿using Il2Cpp;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Seed_Rain_Overhaul
+{
+	internal class CreatePlant_Patch
+	{
+		[HarmonyPatch(typeof(CreatePlant))]
+		public static class Patch
+		{
+			[HarmonyPrefix]
+			[HarmonyPatch("Lim")]
+			private static bool Lim(ref bool __result)
+			{
+				__result = false;
+				return false;
+			}
+
+			[HarmonyPrefix]
+			[HarmonyPatch("LimTravel")]
+			private static bool LimTravel(ref bool __result)
+			{
+				__result = false;
+				return false;
+			}
+		}
+	}
+}
