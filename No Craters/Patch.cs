d41d2276380acd4934f68c888e2d52a955505249
Nero Dynamo <dynamixus0@outlook.com>
2024-12-23 01:19:01 +0800

    New addon: No Craters

---
+++ No Craters/Patch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+namespace No_Craters
+{
+    internal class Patch
+    {
+        [HarmonyPatch(typeof(DoomShroom))]
+        public class DoomShroom_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("AnimExplode")]
+            private static void AnimExplode(DoomShroom __instance)
+            {
+                __instance.Die(Plant.DieReason.Default);
+            }
+        }
+
+        [HarmonyPatch(typeof(IceDoom))]
+        public class IceDoom_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("AnimExplode")]
+            private static void AnimExplode(IceDoom __instance)
+            {
+                __instance.Die(Plant.DieReason.Default);
+            }
+        }
+
+        [HarmonyPatch(typeof(Board))]
+        public class Pit
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("SetDoom")]
+            private static void SetDoom(ref bool setPit)
+            {
+                setPit = false;
+            }
+        }
+    }
+}
