a76a07e0823f16c88671d0e42ee7093c04c74e41
TuanAnh2901 <tuananh4c.ltk@gmail.com>
2025-02-08 14:38:08 +0700

    Update to 2.2.1

--- Plant Conveyor/Classes/Core.cs
+++ Plant Conveyor/Classes/Core.cs
@@ -14,79 +14,85 @@ namespace PlantConveyor
         public const string AUTHOR = "Dynaslash & Climeron";
     }
 
-	public class Core : MelonMod
+    public class Core : MelonMod
     {
-		public static Core Instance { get; private set; }
-		public int FirstCardIndex { get; private set; }
-		public List<int> PlantIDs { get; internal set; }
-		public enum SidesEnum { Right = -1, None = 0, Left = 1}
-		private readonly float _switchTimeout = 0.1f;
-		public float SwitchTimeout => _switchTimeout;
-		public float Countdown { get; private set; }
-		public bool IsSwitching { get; private set; }
+        public static Core Instance { get; private set; }
+        public int FirstCardIndex { get; private set; }
+        public List<int> PlantIDs { get; internal set; }
+        public enum SidesEnum { Right = -1, None = 0, Left = 1 }
+        private readonly float _switchTimeout = 0.001f;
+        public float SwitchTimeout => _switchTimeout;
+        public float Countdown { get; private set; }
+        public bool IsSwitching { get; private set; }
 
-		public override void OnInitializeMelon()
-		{
-			base.OnInitializeMelon();
-			Instance = this;
-		}
-		public override void OnUpdate()
-		{
-			base.OnUpdate();
-			CheckForCDReset();
-			CheckForPlantReplacing();
-		}
-		private void CheckForPlantReplacing()
-		{
-			if (!DecreaseCountdownTimer() || !Board.Instance || !InGameUIMgr.Instance || InGameUIMgr.Instance.cardOnBank == null)
-				return;
-			SidesEnum side = GetSideByKeyHolding();
-			if (side != SidesEnum.None)
-			{
-				Countdown = SwitchTimeout;
-				FirstCardIndex += (int)side;
-				GameObject[] cardsArray = InGameUIMgr.Instance.cardOnBank.Where(gameObject => gameObject).ToArray();
-				for (int i = 0; i < cardsArray.Length; i++)
-					SetPlantCard(cardsArray, i);
-			}
-		}
-		/// <summary>Decreases the <see cref="Countdown"/> by <see cref="Time.deltaTime"/>.</summary>
-		/// <returns>true - if the counter was set to 0; false - otherwise.</returns>
-		private bool DecreaseCountdownTimer()
-		{
-			if (Countdown > 0)
-				Countdown = Countdown < Time.deltaTime ? 0 : Countdown - Time.deltaTime;
-			return Countdown == 0;
-		}
-		private SidesEnum GetSideByKeyHolding()
+        public override void OnInitializeMelon()
         {
-			int sideIndex = 0;
-			if (Input.GetKey(KeyCode.LeftArrow))
-				sideIndex++;
-			if (Input.GetKey(KeyCode.RightArrow))
-				sideIndex--;
-			if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-				IsSwitching = true;
-			return (SidesEnum)sideIndex;
-		}
-		private void SetPlantCard(GameObject[] cardsArray, int index)
+            base.OnInitializeMelon();
+            Instance = this;
+        }
+        public override void OnUpdate()
         {
-			int plantIndex = ((FirstCardIndex + index) % PlantIDs.Count + PlantIDs.Count) % PlantIDs.Count;
-			CardUI component = cardsArray[index].GetComponent<CardUI>();
-			Mouse.Instance.ChangeCardSprite((MixData.PlantType)PlantIDs[plantIndex], component);
-			component.theSeedType = PlantIDs[plantIndex];
-			component.theSeedCost = 0;
-		}
-		private void CheckForCDReset()
+            base.OnUpdate();
+            CheckForCDReset();
+            CheckForPlantReplacing();
+        }
+        private void CheckForPlantReplacing()
         {
-			if (IsSwitching && InGameUIMgr.Instance && InGameUIMgr.Instance.cardOnBank != null)
-			{
-				InGameUIMgr.Instance.cardOnBank
-					.Where(gameObject => gameObject)
-					.Select(gameObject => gameObject.GetComponent<CardUI>())
-					.ToList()
-					.ForEach(card => card.CD = card.fullCD);
-			}
-		}
-	}
+            if (!DecreaseCountdownTimer() || !Board.Instance || !InGameUIMgr.Instance || InGameUIMgr.Instance.cardOnBank == null)
+                return;
+            SidesEnum side = GetSideByKeyHolding();
+            if (side != SidesEnum.None)
+            {
+                Countdown = SwitchTimeout;
+                FirstCardIndex += (int)side;
+                GameObject[] cardsArray = InGameUIMgr.Instance.cardOnBank.Where(gameObject => gameObject).ToArray();
+                for (int i = 0; i < cardsArray.Length; i++)
+                    SetPlantCard(cardsArray, i);
+            }
+        }
+        /// <summary>Decreases the <see cref="Countdown"/> by <see cref="Time.deltaTime"/>.</summary>
+        /// <returns>true - if the counter was set to 0; false - otherwise.</returns>
+        private bool DecreaseCountdownTimer()
+        {
+            if (Countdown > 0)
+                Countdown = Countdown < Time.deltaTime ? 0 : Countdown - Time.deltaTime;
+            return Countdown == 0;
+        }
+        private SidesEnum GetSideByKeyHolding()
+        {
+            int sideIndex = 0;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                sideIndex += 13;
+            if (Input.GetKey(KeyCode.RightArrow))
+                sideIndex -= 13;
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+                IsSwitching = true;
+            return (SidesEnum)sideIndex;
+        }
+
+        private void SetPlantCard(GameObject[] cardsArray, int index)
+        {
+            int plantIndex = ((FirstCardIndex + index) % PlantIDs.Count + PlantIDs.Count) % PlantIDs.Count;
+            CardUI component = cardsArray[index].GetComponent<CardUI>();
+            Mouse.Instance.ChangeCardSprite((PlantType)PlantIDs[plantIndex], component);
+            component.theSeedType = PlantIDs[plantIndex];
+            component.thePlantType = (PlantType)PlantIDs[plantIndex];
+            component.theSeedCost = -1000;
+
+            InGameUIMgr.Instance.cardOnBank[index] = cardsArray[index];
+
+            Debug.Log($"Card {index}: ID={PlantIDs[plantIndex]}, Type={(PlantType)PlantIDs[plantIndex]}");
+        }
+        private void CheckForCDReset()
+        {
+            if (IsSwitching && InGameUIMgr.Instance && InGameUIMgr.Instance.cardOnBank != null)
+            {
+                InGameUIMgr.Instance.cardOnBank
+                    .Where(gameObject => gameObject)
+                    .Select(gameObject => gameObject.GetComponent<CardUI>())
+                    .ToList()
+                    .ForEach(card => card.CD = card.fullCD);
+            }
+        }
+    }
 }
--- Utilities/Utility.cs
+++ Utilities/Utility.cs
@@ -5,204 +5,205 @@ using System.Text;
 
 namespace Utilities
 {
-	internal class Utility
-	{
-		public enum UtilityType
-		{
-			UnliSun, // F1 Done
-			UnliCoins, // F2 Done
-			NoCooldown, // F3 Done
-			InvulPlants, // F4 Done
-			InvulZombies, // F5 Done
-			DoubleDamage, // F6 Done
-			SuperDamage, // F7 Done
-			StopZombieSpawn, // F8 Done
-			StopGameOver, // F9 Done
-			PlantEverywhere, // F10 Done
-			DeveloperMode, // F11 Done
-			ShowUtilities, // F12 Done
-			ColumnPlants, // Semicolon Done
-			ScaredyDream, // Quote 
-			SeedRain, // Backslash 
-			
-			GenerateTrophy, // 0
-			GenerateFertilizer, // 1
-			GenerateBucket, // 2
-			GenerateHelmet, // 3
-			GenerateJack, // 4
-			GeneratePickaxe, // 5
-			GenerateMecha, // 6
-			GenerateSuperMecha, // 5 not the numpad
-			GenerateMeteor, // 7
-			CharmAll, // 8
-			KillAllZombies, // 9
-			KillAllPlants, // +
-			
-		}
+    internal class Utility
+    {
+        public enum UtilityType
+        {
+            UnliSun, // F1 Done
+            UnliCoins, // F2 Done
+            NoCooldown, // F3 Done
+            InvulPlants, // F4 Done
+            InvulZombies, // F5 Done
+            DoubleDamage, // F6 Done
+            SuperDamage, // F7 Done
+            StopZombieSpawn, // F8 Done
+            StopGameOver, // F9 Done
+            PlantEverywhere, // F10 Done
+            DeveloperMode, // F11 Done
+            ShowUtilities, // F12 Done
+            ColumnPlants, // Semicolon Done
+            ScaredyDream, // Quote 
+            SeedRain, // Backslash 
 
-		public enum GenerateType
-		{
-			GenerateTrophy, // 0
-			GenerateFertilizer, // 1
-			GenerateBucket, // 2
-			GenerateHelmet, // 3
-			GenerateJack, // 4
-			GeneratePickaxe, // 5
-			GenerateMecha, // 6
-			GenerateSuperMecha, // 5 not the numpad
-			GenerateMeteor, // 7
-			CharmAll, // 8
-			KillAllZombies, // 9
-			KillAllPlants, // +
-		}
+            GenerateTrophy, // 0
+            GenerateFertilizer, // 1
+            GenerateBucket, // 2
+            GenerateHelmet, // 3
+            GenerateJack, // 4
+            GeneratePickaxe, // 5
+            GenerateMecha, // 6
+            GenerateSuperMecha, // 5 not the numpad
+            GenerateMeteor, // 7
+            CharmAll, // 8
+            KillAllZombies, // 9
+            KillAllPlants, // +
 
-		private class UtilityFeature
-		{
-			public string Name { get; private set; }
-			public UtilityType UtilityType { get; private set; }
-			public KeyCode KeyCode { get; private set; }
-			public bool IsActive { get; set; }
+        }
 
-			public UtilityFeature(string Name, UtilityType UtilityType, KeyCode KeyCode, bool defaultValue = false)
-			{
-				this.Name = Name;
-				this.UtilityType = UtilityType;
-				this.KeyCode = KeyCode;
-				this.IsActive = defaultValue;
-			}
+        public enum GenerateType
+        {
+            GenerateTrophy, // 0
+            GenerateFertilizer, // 1
+            GenerateBucket, // 2
+            GenerateHelmet, // 3
+            GenerateJack, // 4
+            GeneratePickaxe, // 5
+            GenerateMecha, // 6
+            GenerateSuperMecha, // 5 not the numpad
+            GenerateMeteor, // 7
+            CharmAll, // 8
+            KillAllZombies, // 9
+            KillAllPlants, // +
+        }
 
-			public void ToggleUtility()
-			{
+        private class UtilityFeature
+        {
+            public string Name { get; private set; }
+            public UtilityType UtilityType { get; private set; }
+            public KeyCode KeyCode { get; private set; }
+            public bool IsActive { get; set; }
 
-				IsActive = !IsActive;
+            public UtilityFeature(string Name, UtilityType UtilityType, KeyCode KeyCode, bool defaultValue = false)
+            {
+                this.Name = Name;
+                this.UtilityType = UtilityType;
+                this.KeyCode = KeyCode;
+                this.IsActive = defaultValue;
+            }
 
-				if (this.UtilityType == UtilityType.GenerateTrophy || this.UtilityType == UtilityType.GenerateFertilizer || this.UtilityType == UtilityType.GenerateBucket || this.UtilityType == UtilityType.GenerateHelmet || this.UtilityType == UtilityType.GenerateJack || this.UtilityType == UtilityType.GeneratePickaxe || this.UtilityType == UtilityType.GenerateMecha || this.UtilityType == UtilityType.GenerateSuperMecha || this.UtilityType == UtilityType.GenerateMeteor)
-				{
-					return;
-				}
+            public void ToggleUtility()
+            {
+
+                IsActive = !IsActive;
+
+                if (this.UtilityType == UtilityType.GenerateTrophy || this.UtilityType == UtilityType.GenerateFertilizer || this.UtilityType == UtilityType.GenerateBucket || this.UtilityType == UtilityType.GenerateHelmet || this.UtilityType == UtilityType.GenerateJack || this.UtilityType == UtilityType.GeneratePickaxe || this.UtilityType == UtilityType.GenerateMecha || this.UtilityType == UtilityType.GenerateSuperMecha || this.UtilityType == UtilityType.GenerateMeteor)
+                {
+                    return;
+                }
 
                 if (this.UtilityType == UtilityType.CharmAll || this.UtilityType == UtilityType.KillAllPlants || this.UtilityType == UtilityType.KillAllZombies)
                 {
                     return;
                 }
 
-				if (this.UtilityType == UtilityType.ShowUtilities)
-					return;
+                if (this.UtilityType == UtilityType.ShowUtilities)
+                    return;
 
-				Core.ShowToast(string.Format("{0} [{1}]", this.Name, IsActive ? "ON" : "OFF"));
-			}
+                Core.ShowToast(string.Format("{0} [{1}]", this.Name, IsActive ? "ON" : "OFF"));
+            }
 
-			public override string ToString()
-			{
-				if (this.UtilityType == UtilityType.ShowUtilities)
-				{
-					return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
-				}
+            public override string ToString()
+            {
+                if (this.UtilityType == UtilityType.ShowUtilities)
+                {
+                    return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
+                }
 
-				if (this.UtilityType == UtilityType.GenerateTrophy || this.UtilityType == UtilityType.GenerateFertilizer || this.UtilityType == UtilityType.GenerateBucket || this.UtilityType == UtilityType.GenerateHelmet || this.UtilityType == UtilityType.GenerateJack || this.UtilityType == UtilityType.GeneratePickaxe || this.UtilityType == UtilityType.GenerateMecha || this.UtilityType == UtilityType.GenerateSuperMecha || this.UtilityType == UtilityType.GenerateMeteor)
-				{
-					return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
-				}
+                if (this.UtilityType == UtilityType.GenerateTrophy || this.UtilityType == UtilityType.GenerateFertilizer || this.UtilityType == UtilityType.GenerateBucket || this.UtilityType == UtilityType.GenerateHelmet || this.UtilityType == UtilityType.GenerateJack || this.UtilityType == UtilityType.GeneratePickaxe || this.UtilityType == UtilityType.GenerateMecha || this.UtilityType == UtilityType.GenerateSuperMecha || this.UtilityType == UtilityType.GenerateMeteor)
+                {
+                    return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
+                }
 
-				if (this.UtilityType == UtilityType.CharmAll || this.UtilityType == UtilityType.KillAllPlants || this.UtilityType == UtilityType.KillAllZombies)
-				{
-					return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
-				}
+                if (this.UtilityType == UtilityType.CharmAll || this.UtilityType == UtilityType.KillAllPlants || this.UtilityType == UtilityType.KillAllZombies)
+                {
+                    return string.Format("[{0}] {1}", this.KeyCode.ToString(), this.Name);
+                }
 
-				return string.Format("[{0}] {1} [{2}]", this.KeyCode.ToString(), this.Name, IsActive ? "ON" : "OFF");
-			}
-		}
+                return string.Format("[{0}] {1} [{2}]", this.KeyCode.ToString(), this.Name, IsActive ? "ON" : "OFF");
+            }
+        }
 
-		private static Dictionary<UtilityType, UtilityFeature> utilityLists = new Dictionary<UtilityType, UtilityFeature>()
-		{
-			{UtilityType.UnliSun, new UtilityFeature("Unlimited Sun", UtilityType.UnliSun, KeyCode.F1)},
-			{UtilityType.UnliCoins, new UtilityFeature("Unlimited Coins", UtilityType.UnliCoins, KeyCode.F2)},
-			{UtilityType.NoCooldown, new UtilityFeature("No Cooldown", UtilityType.NoCooldown, KeyCode.F3)},
-			{UtilityType.InvulPlants, new UtilityFeature("Invulnerable Plants", UtilityType.InvulPlants, KeyCode.F4)},
-			{UtilityType.InvulZombies, new UtilityFeature("Invulnerable Zombies", UtilityType.InvulZombies, KeyCode.F5)},	
-			{UtilityType.DoubleDamage, new UtilityFeature("Double Plant Damage", UtilityType.DoubleDamage, KeyCode.F6)},
-			{UtilityType.SuperDamage, new UtilityFeature("10x Plant Damage", UtilityType.SuperDamage, KeyCode.F7)},
-			{UtilityType.StopZombieSpawn, new UtilityFeature("Stop Zombie Spawn", UtilityType.StopZombieSpawn, KeyCode.F8)},
-			{UtilityType.StopGameOver, new UtilityFeature("Stop Game Over", UtilityType.StopGameOver, KeyCode.F9)},
-			{UtilityType.PlantEverywhere, new UtilityFeature("Plant Everywhere", UtilityType.PlantEverywhere, KeyCode.F10)},
-			{UtilityType.DeveloperMode, new UtilityFeature("Developer Mode", UtilityType.DeveloperMode, KeyCode.F11)},
+        private static Dictionary<UtilityType, UtilityFeature> utilityLists = new Dictionary<UtilityType, UtilityFeature>()
+        {
+            {UtilityType.UnliSun, new UtilityFeature("Unlimited Sun", UtilityType.UnliSun, KeyCode.F1)},
+            {UtilityType.UnliCoins, new UtilityFeature("Unlimited Coins", UtilityType.UnliCoins, KeyCode.F2)},
+            {UtilityType.NoCooldown, new UtilityFeature("No Cooldown", UtilityType.NoCooldown, KeyCode.F3)},
+            {UtilityType.InvulPlants, new UtilityFeature("Invulnerable Plants", UtilityType.InvulPlants, KeyCode.F4)},
+            {UtilityType.InvulZombies, new UtilityFeature("Invulnerable Zombies", UtilityType.InvulZombies, KeyCode.F5)},
+            {UtilityType.DoubleDamage, new UtilityFeature("Double Plant Damage", UtilityType.DoubleDamage, KeyCode.F6)},
+            {UtilityType.SuperDamage, new UtilityFeature("10x Plant Damage", UtilityType.SuperDamage, KeyCode.F7)},
+            {UtilityType.StopZombieSpawn, new UtilityFeature("Stop Zombie Spawn", UtilityType.StopZombieSpawn, KeyCode.F8)},
+            {UtilityType.StopGameOver, new UtilityFeature("Stop Game Over", UtilityType.StopGameOver, KeyCode.F9)},
+            {UtilityType.PlantEverywhere, new UtilityFeature("Plant Everywhere", UtilityType.PlantEverywhere, KeyCode.F10)},
+            {UtilityType.DeveloperMode, new UtilityFeature("Developer Mode", UtilityType.DeveloperMode, KeyCode.F11)},
 
-			{UtilityType.ColumnPlants, new UtilityFeature("Column Plants", UtilityType.ColumnPlants, KeyCode.Semicolon)},
-			{UtilityType.ScaredyDream, new UtilityFeature("Scaredy Dream", UtilityType.ScaredyDream, KeyCode.Quote)},
-			{UtilityType.SeedRain, new UtilityFeature("Seed Rain", UtilityType.SeedRain, KeyCode.Backslash)},
+            {UtilityType.ColumnPlants, new UtilityFeature("Column Plants", UtilityType.ColumnPlants, KeyCode.Semicolon)},
+            {UtilityType.ScaredyDream, new UtilityFeature("Scaredy Dream", UtilityType.ScaredyDream, KeyCode.Quote)},
+            {UtilityType.SeedRain, new UtilityFeature("Seed Rain", UtilityType.SeedRain, KeyCode.Backslash)},
 
-			{UtilityType.GenerateTrophy, new UtilityFeature("Generate Trophy", UtilityType.GenerateTrophy, KeyCode.Keypad0)},
-			{UtilityType.GenerateFertilizer, new UtilityFeature("Generate Garden Sprout (Not Working)", UtilityType.GenerateFertilizer, KeyCode.Keypad1)},
-			{UtilityType.GenerateBucket, new UtilityFeature("Generate Bucket", UtilityType.GenerateBucket, KeyCode.Keypad2)},
-			{UtilityType.GenerateHelmet, new UtilityFeature("Generate Helmet", UtilityType.GenerateHelmet, KeyCode.Keypad3)},
-			{UtilityType.GenerateJack, new UtilityFeature("Generate Jack-in-the-Box", UtilityType.GenerateJack, KeyCode.Keypad4)},
-			{UtilityType.GeneratePickaxe, new UtilityFeature("Generate Pickaxe", UtilityType.GeneratePickaxe, KeyCode.Keypad5)},
-			{UtilityType.GenerateMecha, new UtilityFeature("Generate Mecha Fragment", UtilityType.GenerateMecha, KeyCode.Keypad6)},
-			{UtilityType.GenerateSuperMecha, new UtilityFeature("Generate Giga Mecha Fragment", UtilityType.GenerateSuperMecha, KeyCode.Alpha5)},
-			{UtilityType.GenerateMeteor, new UtilityFeature("Generate Meteor", UtilityType.GenerateMeteor, KeyCode.Keypad7)},
-			{UtilityType.CharmAll, new UtilityFeature("Charm All Zombies", UtilityType.CharmAll, KeyCode.Keypad8)},
-			{UtilityType.KillAllZombies, new UtilityFeature("Kill All Zombies", UtilityType.KillAllZombies, KeyCode.Keypad9)},
-			{UtilityType.KillAllPlants, new UtilityFeature("Kill All Plants", UtilityType.KillAllPlants, KeyCode.KeypadPlus)},
+            {UtilityType.GenerateTrophy, new UtilityFeature("Generate Trophy", UtilityType.GenerateTrophy, KeyCode.Keypad0)},
+            {UtilityType.GenerateFertilizer, new UtilityFeature("Generate Garden Sprout (Not Working)", UtilityType.GenerateFertilizer, KeyCode.Keypad1)},
+            {UtilityType.GenerateBucket, new UtilityFeature("Generate Bucket", UtilityType.GenerateBucket, KeyCode.Keypad2)},
+            {UtilityType.GenerateHelmet, new UtilityFeature("Generate Helmet", UtilityType.GenerateHelmet, KeyCode.Keypad3)},
+            {UtilityType.GenerateJack, new UtilityFeature("Generate Jack-in-the-Box", UtilityType.GenerateJack, KeyCode.Keypad4)},
+            {UtilityType.GeneratePickaxe, new UtilityFeature("Generate Pickaxe", UtilityType.GeneratePickaxe, KeyCode.Keypad5)},
+            {UtilityType.GenerateMecha, new UtilityFeature("Generate Mecha Fragment", UtilityType.GenerateMecha, KeyCode.Keypad6)},
+            {UtilityType.GenerateSuperMecha, new UtilityFeature("Generate Giga Mecha Fragment", UtilityType.GenerateSuperMecha, KeyCode.Alpha5)},
+            {UtilityType.GenerateMeteor, new UtilityFeature("Generate Meteor", UtilityType.GenerateMeteor, KeyCode.Keypad7)},
+            {UtilityType.CharmAll, new UtilityFeature("Charm All Zombies", UtilityType.CharmAll, KeyCode.Keypad8)},
+            {UtilityType.KillAllZombies, new UtilityFeature("Kill All Zombies", UtilityType.KillAllZombies, KeyCode.Keypad9)},
+            {UtilityType.KillAllPlants, new UtilityFeature("Kill All Plants", UtilityType.KillAllPlants, KeyCode.KeypadPlus)},
 
-			{UtilityType.ShowUtilities, new UtilityFeature("Utilities List", UtilityType.ShowUtilities, KeyCode.F12, false)},
-		};
 
-		public static string GetUtilities()
-		{
-			StringBuilder status = new StringBuilder();
-			status.AppendLine("Utilities: ");
-			foreach (var utility in utilityLists.Values)
-			{
-				status.AppendLine(utility.ToString());
-			}
-			return status.ToString();
-		}
+            {UtilityType.ShowUtilities, new UtilityFeature("Utilities List", UtilityType.ShowUtilities, KeyCode.F12, false)},
+        };
 
-		public static void ToggleUtility(UtilityType UtilityType)
-		{
-			if (!utilityLists.ContainsKey(UtilityType))
-				return;
+        public static string GetUtilities()
+        {
+            StringBuilder status = new StringBuilder();
+            status.AppendLine("Utilities: ");
+            foreach (var utility in utilityLists.Values)
+            {
+                status.AppendLine(utility.ToString());
+            }
+            return status.ToString();
+        }
 
-			utilityLists[UtilityType].ToggleUtility();
-		}
+        public static void ToggleUtility(UtilityType UtilityType)
+        {
+            if (!utilityLists.ContainsKey(UtilityType))
+                return;
 
-		public static bool GetActive(UtilityType UtilityType)
-		{
-			if (!utilityLists.ContainsKey(UtilityType))
-				return false;
+            utilityLists[UtilityType].ToggleUtility();
+        }
 
-			return utilityLists[UtilityType].IsActive;
-		}
+        public static bool GetActive(UtilityType UtilityType)
+        {
+            if (!utilityLists.ContainsKey(UtilityType))
+                return false;
 
-		public static void SetActive(UtilityType UtilityType, bool value)
-		{
-			if (!utilityLists.ContainsKey(UtilityType))
-				return;
+            return utilityLists[UtilityType].IsActive;
+        }
 
-			utilityLists[UtilityType].IsActive = value;
-		}
+        public static void SetActive(UtilityType UtilityType, bool value)
+        {
+            if (!utilityLists.ContainsKey(UtilityType))
+                return;
 
-		public static void OnLateUpdate()
-		{
-			foreach (var (type, utility) in utilityLists)
-			{
-				if (utility.KeyCode != KeyCode.None && Enum.IsDefined(typeof(KeyCode), utility.KeyCode))
-				{
-					if (Input.GetKeyDown(utility.KeyCode))
-					{
-						utility.ToggleUtility();
-					}
-				}
-			}
-		}
-		public static void SpawnItem(string resourcePath)
-		{
-			GameObject gameObject = Resources.Load<GameObject>(resourcePath);
-			if (gameObject != null)
-			{
-				UnityEngine.Object.Instantiate<GameObject>(gameObject, new Vector2(0f, 0f), Quaternion.identity, GameAPP.board.transform);
-				return;
-			}
-		}
-	}
+            utilityLists[UtilityType].IsActive = value;
+        }
+
+        public static void OnLateUpdate()
+        {
+            foreach (var (type, utility) in utilityLists)
+            {
+                if (utility.KeyCode != KeyCode.None && Enum.IsDefined(typeof(KeyCode), utility.KeyCode))
+                {
+                    if (Input.GetKeyDown(utility.KeyCode))
+                    {
+                        utility.ToggleUtility();
+                    }
+                }
+            }
+        }
+        public static void SpawnItem(string resourcePath)
+        {
+            GameObject gameObject = Resources.Load<GameObject>(resourcePath);
+            if (gameObject != null)
+            {
+                UnityEngine.Object.Instantiate<GameObject>(gameObject, new Vector2(0f, 0f), Quaternion.identity, GameAPP.board.transform);
+                return;
+            }
+        }
+    }
 }
